533c9ac9e0999209acade629412176d53aab9a32
David E Bratton <debratton@Davids-MacBook-Pro.local>
2022-08-10 11:24:49 -0500

    Create XCode Project

---
+++ Twitter/Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by David E Bratton on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
